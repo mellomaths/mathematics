e8c06e8fdeba247268f68187d980364405cb7ce4
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-01 21:37:26 -0300

    Add congruence relation function

--- algebra.py
+++ algebra.py
@@ -6,9 +6,30 @@ def greatest_common_divisor(a, b):
   a -- an integer
   b -- another integer
   """
+
   remainder = a % b
   if (remainder == 0):
     return b
     
   return abs(greatest_common_divisor(b, remainder))
 
+
+def has_congruence_relation(a, b, n):
+  """
+  Defines if the integer a and the integer b has a congruence relation modulo n.
+
+  Rule:
+  For a given positive integer n, two integers a and b are called congruent modulo n
+  if a-b is divisible by n (or equivalently if a and b have the same remainder when divided by n).
+
+  This algorithm uses the second rule, calculating the remainders and comparing.
+
+  Keyword arguments:
+  a -- an integer
+  b -- another integer
+  n -- another integer (modulo)
+  """
+
+  remainder_a = a % n
+  remainder_b = b % n
+  return remainder_a == remainder_b
