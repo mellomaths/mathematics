d3fef694d33ed6dd60b3772b64c737b74d5d4d3a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-02 12:28:08 -0300

    Add docstring and pattern for distance function

--- algebra.py
+++ algebra.py
@@ -63,13 +63,22 @@ def prime(n) :
 
 
 def distance(x1, y1, z1, x2, y2, z2) :
-    """
-    Calculates the distance between two points in space
-    """
-    import math
-    distX = (x2 - x1) * (x2 - x1)
-    distY = (y2 - y1) * (y2 - y1)
-    distZ = (z2 - z1) * (z2 - z1)
-    distance = math.sqrt(distX + distY + distZ)
-    return distance
+  """
+  Calculates the distance between two points in space (3D).
+  To use this function in 2D, pass both z1 and z2 as 0.
+
+  Keyword arguments:
+  x1 - x position of point 1
+  y1 - y position of point 1
+  z1 - z position of point 1
+  x2 - x position of point 2
+  y2 - y position of point 2
+  z2 - z position of point 2
+  """
+
+  distance_x = (x2 - x1) * (x2 - x1)
+  distance_y = (y2 - y1) * (y2 - y1)
+  distance_z = (z2 - z1) * (z2 - z1)
+  distance = math.sqrt(distance_x + distance_y + distance_z)
+  return distance
 
