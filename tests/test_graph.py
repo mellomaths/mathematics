cb3946b929518aefc3f16662d39617f4e5f4cd7a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-28 18:09:32 -0300

    Add tests for graph representations

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -5,7 +5,6 @@ import random
 
 class TestGraph(unittest.TestCase):
 
-
     def setUp(self) -> None:
         self.number_of_vertices = 8
         self.edges = [
@@ -44,6 +43,43 @@ class TestGraph(unittest.TestCase):
         graph = self.graph
         self.assertListEqual(graph.degrees(), [3, 3, 3, 3, 3, 3, 3, 3])
         self.assertEqual(graph.node_degree(1), 3)
+
+    def test_graph_representation_incidence_matrix(self):
+        graph = self.graph
+        matrix = [
+            [0, 1, 0, 1, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0, 0, 1, 0],
+            [1, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 1, 0, 0, 1], 
+            [0, 0, 1, 0, 1, 0, 0, 1], 
+            [0, 0, 0, 1, 0, 1, 1, 0]
+        ]
+        self.assertEqual(len(graph.representation.incidence_matrix()), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[0]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[1]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[2]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[3]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[4]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[5]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[6]), 8)
+        self.assertTrue(len(graph.representation.incidence_matrix()[7]), 8)
+        self.assertEqual(graph.representation.incidence_matrix(), matrix)
+    
+    def test_graph_representation_adjacency_list(self):
+        graph = self.graph
+        adj_list = [
+            [1, 3, 4], 
+            [0, 2, 5], 
+            [1, 3, 6], 
+            [0, 2, 7], 
+            [0, 5, 6], 
+            [1, 4, 7], 
+            [2, 4, 7], 
+            [3, 5, 6]
+        ]
+        self.assertEqual(graph.representation.adjacency_list(), adj_list)
 
 
 if __name__ == "__main__":
